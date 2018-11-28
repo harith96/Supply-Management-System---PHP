e067337b94b912e45c84d356551e3fc8e5566032
Isuru Maduranga <160711l@uom.lk>
2018-11-28 10:01:46 +0530

    added product registration

---
+++ products/Inc/connection.inc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Harith
+ * Date: 11/27/2018
+ * Time: 7:37 PM
+ */
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=sms", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully <br>";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $pdo->errorInfo(2);
+}
+?>
