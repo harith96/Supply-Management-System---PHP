2b60b67609f32f1015e02d9e317118173d3505a4
bGuom <32662218+bGuom@users.noreply.github.com>
2018-11-29 03:19:54 +0530

    View orders

---
+++ vieworders/order_receive.php
@@ -0,0 +1,14 @@
+<?php
+	include_once('db.php');	
+
+	$result=mysqli_query($db,"SELECT * FROM orders");
+	
+	$rows = array();
+
+	while ($r = mysqli_fetch_assoc($result)) {
+		array_push($rows,$r);
+	}
+
+	echo json_encode($rows);
+	
+?>
